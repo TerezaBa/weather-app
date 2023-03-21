998bdf18e142bd2444d9cc070a7695c0f382555f
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2023-03-21 16:49:24 +0100

    Implemented forecast using API

--- src/app.js
+++ src/app.js
@@ -19,36 +19,55 @@ function formateDate(timeStamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formateDay(timeStamp) {
+  let date = new Date(timeStamp);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day = days[date.getDay()];
+  return day;
+}
+
 function displayForecast(response) {
-  let forecastCard = document.querySelector("#forecast");
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
 
   let forecastHTML = `<div class="row">`;
-  let forecastDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  forecastDays.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">${forecastDay}</h5>
-                  <img
-                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAepJREFUaN7tmd2thCAQhSnBEijBEmjgJpZgCZZgCZZgCXZwLcFX3yiBDlhIhhvC5dcdFzeB5LzsTtjz4cwwZomUknyzSANoAA2gATQANICSdZ4nA/Wk0ioGUGY7pVVJKElLXGl6NIAySMGojGh9MsCRMG80Pw5AmRozzRvppzVoGNCk068mwFYI4JPArpMSgB0BwGj5doAVutnsdDNdYwwVAH5o8rTNqzoyGsKIAqAvKETjRkNGQxC5BR8EuMm8vJCO0cKPAXBs83DyXcF9YmvKBoDHfIf5/o2DESUAM7L5HU6eez4fraFwzEwvYQo9BLDfcPrM2dek02jd1vZnOfuyEMCKaH52OwqMGaF04vBdDsQaApgQT546Jy9g/1gtcHgSqUxYQgD0xrwvgR9gn5D6WBtdEC6s6e55KQbQWd3hijqECVakxoq33okzXj03pFpiNQCOT4zfSYCf35MprUpHaBMrhltzlKwOoAx1SkJJgnqPeR3DrRhmXYZY6q8CLJaxP3NOzOzEjOSDKwigT9sxtnvM01RMTYDdMUc9AG5M/wgAnQaOsdljfnBiFlJh/QPwFK4u0C5RuMKNqQngFu7wtMJNAYhUUebE1ATYYoULAFvNwk0BmFuVZty8lFRe7S+mBtAAGkADeFsvKpKWeAy6FowAAAAASUVORK5CYII="
-                    alt="Cloudy"
-                    width="36"
-                  />
-                  <span class="forecast-temp-max">7°</span>
-                  <span class="forecast-temp-min">3°</span>
-                </div>
-              </div>
-            </div>`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index < 7) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+    <div class="card">
+    <div class="card-body">
+    <h5 class="card-title">${formateDay(forecastDay.time * 1000)}</h5>
+                <img
+                src=${forecastDay.condition.icon_url}
+                alt=${forecastDay.condition.icon}
+                width="36"
+                />
+                <span class="forecast-temp-max">${Math.round(
+                  forecastDay.temperature.maximum
+                )}°</span>
+                  <span class="forecast-temp-min">${Math.round(
+                    forecastDay.temperature.minimum
+                  )}°</span>
+                    </div>
+                    </div>
+                    </div>`;
+    }
   });
-
   forecastHTML = forecastHTML + `</div>`;
+
+  let forecastCard = document.querySelector("#forecast");
   forecastCard.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemp(response) {
-  console.log(response);
   document.querySelector("#current-date").innerHTML = formateDate(new Date());
 
   document.querySelector("#city").innerHTML = response.data.city;
@@ -71,9 +90,12 @@ function showTemp(response) {
   document
     .querySelector("#weather-icon")
     .setAttribute("src", `${response.data.condition.icon_url}`);
+
   document
     .querySelector("#weather-icon")
     .setAttribute("alt", `${response.data.condition.icon}`);
+
+  getForecast(response.data.coordinates);
 }
 
 function searchCity(cityName) {
@@ -95,4 +117,3 @@ let apiKey = "b36tedd42903o5c6c68a4a10b4b1953f";
 let unit = "metric";
 
 searchCity("Prague");
-displayForecast();
